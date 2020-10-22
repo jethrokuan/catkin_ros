168643583e8d5ca3725af43f3a33c8bde83b1dad
Jethro Kuan <jethrokuan95@gmail.com>
2020-10-22 17:36:48 +0800

    raise error

--- src/systems/mvp_grasping/src/mvp_grasping/utils.py
+++ src/systems/mvp_grasping/src/mvp_grasping/utils.py
@@ -12,6 +12,8 @@ def correct_grasp(grasp, gripper):
         angle = np.pi/2
     elif gripper == "panda":
         angle = np.pi/4
+    else:
+        raise ValueError("Unsupported gripper: {}".format(gripper))
 
     q_rot = tft.quaternion_from_euler(0, 0, angle)
     q_new = tfh.list_to_quaternion(tft.quaternion_multiply(tfh.quaternion_to_list(grasp.pose.orientation), q_rot))
