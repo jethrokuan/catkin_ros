e0301b77a2eb35530546a2255ff7929ac9cc95d7
Jethro Kuan <jethrokuan95@gmail.com>
2020-10-23 15:48:04 +0800

    fix current_joint_state utility

--- src/systems/mvp_grasping/ros_nodes/panda_current_joint_state.py
+++ src/systems/mvp_grasping/ros_nodes/panda_current_joint_state.py
@@ -7,8 +7,9 @@ from dougsm_helpers.ros_control import ControlSwitcher
 
 if __name__ == '__main__':
     rospy.init_node('panda_open_loop_grasp')
+    gripper = rospy.get_param("~gripper", "panda")
     cs = ControlSwitcher({'moveit': 'position_joint_trajectory_controller',
                           'velocity': 'cartesian_velocity_node_controller'})
     cs.switch_controller('moveit')
-    pc = PandaCommander(group_name='panda_arm', gripper=self.gripper)
+    pc = PandaCommander(group_name='panda_arm', gripper=gripper)
     print(pc.get_current_pose())
